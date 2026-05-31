56f6496428c021eccfe9d139783f0fc58efffa43
Ritesh Raj Pandit <41156634+riteshrajpandit@users.noreply.github.com>
2026-05-31 12:46:24 +0545

    Initial Commit

---
+++ VoiceFlow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VoiceFlow
+//
+//  Created by Ritesh Raj Pandit on 31/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VoiceFlow/VoiceFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceFlowApp.swift
+//  VoiceFlow
+//
+//  Created by Ritesh Raj Pandit on 31/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct VoiceFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
